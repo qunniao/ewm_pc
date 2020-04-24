7befbfc347c987f8fb5e76bcfc1f485004dcd3c2
guojinyu <gjymaster@163.com>
2020-04-25 02:48:02 +0800

    二维码pc端

---
+++ js/method.js
@@ -0,0 +1,7 @@
+function getServerHost() {
+    return "http://admin.zhanchengwlkj.com:8080/product_code_applet";
+}
+
+function getServerHosts() {
+    return "http://admin.zhanchengwlkj.com:8080/product_code_applet";
+}
